eea234bc0758b5f8585667dd346f19d6d610244c
Nur Kholis <nurkholispragaan@gmail.com>
2025-06-16 06:05:11 +0700

    first

---
+++ js/animation.js
@@ -0,0 +1,42 @@
+// console.log('static');
+// setTimeout(() => {
+//     bodymovin.loadAnimation({
+//         container: document.getElementById('animation'),
+//         render: 'svg',
+//         loop: true,
+//         autoplay: true,
+//         path: 'animations/drawkit-grape-animation-4-LOOP.json'
+//     })
+    
+//     bodymovin.loadAnimation({
+//         container: document.getElementById('whyme-1'),
+//         render: 'svg',
+//         loop: true,
+//         autoplay: true,
+//         path: 'animations/drawkit-grape-animation-4-LOOP.json'
+//     })
+    
+//     bodymovin.loadAnimation({
+//         container: document.getElementById('whyme-2'),
+//         render: 'svg',
+//         loop: true,
+//         autoplay: true,
+//         path: 'animations/drawkit-grape-animation-5-LOOP.json'
+//     })
+    
+//     bodymovin.loadAnimation({
+//         container: document.getElementById('whyme-3'),
+//         render: 'svg',
+//         loop: true,
+//         autoplay: true,
+//         path: 'animations/drawkit-grape-animation-8-LOOP.json'
+//     })
+
+//     bodymovin.loadAnimation({
+//         container: document.getElementById('contact-me'),
+//         render: 'svg',
+//         loop: true,
+//         autoplay: true,
+//         path: 'animations/drawkit-grape-animation-4-LOOP.json'
+//     });
+// }, 300);
